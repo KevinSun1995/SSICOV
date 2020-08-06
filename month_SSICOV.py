336798d2114f22bc3d90c91e7f0ae41a0f43a6ba
Canjie Sun <cansun@cs.stonybrook.edu>
2020-08-06 13:37:36 +0800

    1. add month SSICOV processing script 2. keep same dimensions regardless the freedom

--- month_SSICOV.py
+++ month_SSICOV.py
@@ -1,6 +1,7 @@
 import os
 import sys
 import time
+from shutil import rmtree
 
 import numpy as np
 import pandas as pd
@@ -14,21 +15,26 @@ if __name__ == '__main__':
                     python3 month_SSICOV csv_file_path save_csv_path''')
         sys.exit()
     # chunk size = 20min data
-    df = pd.read_csv(sys.argv[1], chunksize=60000)
-    fn_fd = sys.argv[2]
-    if os.path.exists(fn_fd):
-        os.remove(fn_fd)
+    df = pd.read_csv(sys.argv[1], chunksize=180000)
+    result_folder = sys.argv[2]
+    if os.path.exists(result_folder):
+        rmtree(result_folder)
+    os.mkdir(result_folder)
     model = SSICOV(dt=.02, min_order=20, max_order=40)
     for index, chunk in enumerate(df):
         start_time = time.time()
         data = chunk.values
-        h = index // 3
-        m = index % 3 * 20
-        fn, _, _ = model.fit(data)
+        hour = index
+        day = index // 24
+        fn, zeta, _ = model.fit(data)
+        fn_fd = os.path.join(result_folder, 'fn.csv')
+        zeta_fd = os.path.join(result_folder, 'zeta.csv')
         with open(fn_fd, 'a') as fn_file:
             fn = np.array([fn])
-            print(fn.shape)
             np.savetxt(fn_file, fn, delimiter=',')
+        with open(zeta_fd, 'a') as zeta_file:
+            zeta = np.array([zeta])
+            np.savetxt(zeta_file, zeta, delimiter=',')
         end_time = time.time()
-        print('Data From %02d:%02d processed' % (h, m))
+        print('Data From Day:%02d Hour:%02d processed' % (day, hour))
         print('The processing time: %.2f' % (end_time - start_time))
